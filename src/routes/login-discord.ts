9f80fc5f20ef93e3d9c6ad75d86da1082621dd8e
AxiomXDev <agrippineloorette@gmail.com>
2025-11-14 21:14:38 +0100

    update

--- src/routes/login-discord.ts
+++ src/routes/login-discord.ts
@@ -4,7 +4,7 @@ import { DISCORD_CLIENT_ID, redirect } from '../server';
 const router = Router();
 
 router.get('/login-discord', async (req, res) => {
-    res.redirect(`https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(redirect)}&scope=identify`);
+    res.redirect(`https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(redirect)}&scope=identifyidentify%20email`);
 });
 
 export default router;
